da89bcc40886948258d5f783a85705fe1c903d19
ak0327 <ca.fie.0220@gmail.com>
2021-10-09 17:11:01 +0900

    Initial Commit

---
+++ Kadai1/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  Kadai1
+//
+//  Created by AkiraTANIDA on 2021/10/09.
+//
+
+import SwiftUI
+let numOfForms: Int = 5
+
+struct ContentView: View {
+    @State var inputNums: [Int] = Array(repeating: 0, count: numOfForms)
+    @State var ans: String = "Label"
+    
+    var body: some View {
+        ZStack{
+            // 画面をタップするとキーボードを閉じる & 入力が確定する
+            Color.white.opacity(0.01)
+                .edgesIgnoringSafeArea(.all)
+                .onTapGesture {
+                    UIApplication.shared.closeKeyboard()
+                    print("tapped")
+                }
+            
+            // TextField, Button, Labelを配置
+            VStack(alignment: .leading, spacing: 10) {
+                
+                ForEach (0..<5) { index in
+                    TextField("input num",
+                              value: $inputNums[index],
+                              formatter: NumberFormatter())
+                        .onReceive(
+                                NotificationCenter.default.publisher(for: UITextField.textDidEndEditingNotification),
+                                perform: textDidEndEditing)
+                        .keyboardType(.numberPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                }
+                
+                Button(action: {
+                    UIApplication.shared.closeKeyboard()
+                    self.ans = String(inputNums.reduce(0, +))
+                }, label: {
+                    Text("Button")
+                })
+                
+                Text(self.ans)
+                
+            }.padding()
+        }
+
+    }
+    
+    // returnを押さなくても値を反映させる
+    // Ref https://zenn.dev/konomae/articles/2f9182561b1372
+    func textDidEndEditing(_ notification: Notification) {
+        let textField = notification.object as! UITextField
+        textField.sendActions(for: .editingDidEndOnExit)
+    }
+
+}
+
+
+// 画面をタップするとキーボードを閉じる & 入力が確定する
+extension UIApplication {
+    func closeKeyboard() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kadai1/Kadai1App.swift
@@ -0,0 +1,17 @@
+//
+//  Kadai1App.swift
+//  Kadai1
+//
+//  Created by AkiraTANIDA on 2021/10/09.
+//
+
+import SwiftUI
+
+@main
+struct Kadai1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
